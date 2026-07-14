432872a136278d8bce10d424c6e7e6d0e56586ab
Josué Torres <josuejesusts@gmail.com>
2026-07-14 13:01:08 -0500

    feat: add Navbar and Navitems components; create ProfilePage

---
+++ components/Navitems.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Companions", href: "/companions" },
+  { label: "My Journay", href: "/my-journey" },
+  { label: "Sign in", href: "/sign-in" },
+];
+
+const Navitems = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(pathname === href && "text-primary font-semibold")}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Navitems;
